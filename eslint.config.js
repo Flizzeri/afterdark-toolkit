b104a00e046be97ecd1dff05a8572f8b41a39a96
flizzeri <filippo.lizzeri@gmail.com>
2025-11-11 23:17:00 +0100

    refined initial setup to working state

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
 export default {
         root: true,
         parser: '@typescript-eslint/parser',
-        plugins: ['@typescript-eslint'],
+        plugins: ['@typescript-eslint', 'import'],
         extends: [
                 'eslint:recommended',
                 'plugin:@typescript-eslint/recommended',
                 'plugin:@typescript-eslint/stylistic',
+                'plugin:import/errors',
+                'plugin:import/warnings',
+                'plugin:import/typescript',
                 'prettier',
         ],
         parserOptions: {
@@ -14,11 +17,33 @@ export default {
                 project: './tsconfig.base.json',
         },
         rules: {
-                '@typescript-eslint/explicit-function-return-type': 'error',
-                '@typescript-eslint/no-explicit-any': 'error',
-                '@typescript-eslint/consistent-type-imports': 'warn',
+                // Safety & clarity
+                '@typescript-eslint/explicit-function-return-type': ['error'],
+                '@typescript-eslint/no-explicit-any': ['error'],
+                '@typescript-eslint/consistent-type-imports': ['error'],
+                '@typescript-eslint/no-inferrable-types': ['error'],
                 '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-                'no-console': 'warn',
+                '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+                '@typescript-eslint/explicit-member-accessibility': [
+                        'error',
+                        { accessibility: 'explicit' },
+                ],
+                '@typescript-eslint/member-ordering': 'error',
+                'no-console': ['warn', { allow: ['error', 'warn'] }],
+                'import/no-default-export': 'error',
+                'import/order': [
+                        'error',
+                        {
+                                groups: [
+                                        'builtin',
+                                        'external',
+                                        'internal',
+                                        ['parent', 'sibling', 'index'],
+                                ],
+                                'newlines-between': 'always',
+                                alphabetize: { order: 'asc', caseInsensitive: true },
+                        },
+                ],
         },
         ignorePatterns: ['dist', 'node_modules'],
 };
