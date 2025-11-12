e2cf3f7d45c5f08fd6c5ac18610dafe9d276bcd2
flizzeri <filippo.lizzeri@gmail.com>
2025-11-12 10:53:00 +0100

    fix(cli): satisfied eslint

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+
 import { version } from '../package.json';
 
 const program = new Command();
@@ -9,7 +10,7 @@ program.name('adtk').description('Afterdark Toolkit CLI').version(version);
 program.command('validate')
         .description('Run validation tools')
         .action(() => {
-                console.log('Validation command');
+                console.warn('Validation command not yet defined');
         });
 
 program.parse();
